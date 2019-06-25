60cb070f8ec64139191263d41d12eb131c7dcec7
Bulat Aleev <bkaleyev@mts/ru>
2019-06-25 14:29:21 +0300

    changed statuses in provisioning and deprovisioning

--- src/main/kotlin/de/evoila/osb/checker/request/bodies/ProvisionBody.kt
+++ src/main/kotlin/de/evoila/osb/checker/request/bodies/ProvisionBody.kt
@@ -1,70 +1,73 @@
 package de.evoila.osb.checker.request.bodies
 
 import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.core.util.RequestPayload
 import de.evoila.osb.checker.response.Plan
 import de.evoila.osb.checker.response.Service
 import java.util.*
 
 abstract class ProvisionBody : RequestBody {
 
-  data class ValidProvisioning(
-      var service_id: String?,
-      var plan_id: String?,
-      var organization_guid: String? = UUID.randomUUID().toString(),
-      var space_guid: String? = UUID.randomUUID().toString(),
-      @JsonInclude(JsonInclude.Include.NON_NULL)
-      var parameters: Map<String, Any>? = null
-  ) : ProvisionBody() {
+    data class ValidProvisioning(
+            var service_id: String?,
+            var plan_id: String?,
+            var organization_guid: String? = UUID.randomUUID().toString(),
+            var space_guid: String? = UUID.randomUUID().toString(),
+//          var parameters: RequestPayload?,
+            @JsonInclude(JsonInclude.Include.NON_NULL)
+            var parameters: Map<String, Any>? = null //!!! google how to parse app.yml
+    ) : ProvisionBody() {
 
-    constructor(service: Service, plan: Plan) : this(
-        service_id = service.id,
-        plan_id = plan.id
-    )
-  }
+        constructor(service: Service, plan: Plan) : this(
+                service_id = service.id,
+                plan_id = plan.id
 
-  data class NoServiceFieldProvisioning(
-      var service_id: String?,
-      var organization_guid: String? = UUID.randomUUID().toString(),
-      var space_guid: String? = UUID.randomUUID().toString()
-  ) : ProvisionBody() {
+        )
+    }
 
-    constructor(service: Service) : this(
-        service_id = service.id
-    )
-  }
+    data class NoServiceFieldProvisioning(
+            var service_id: String?,
+            var organization_guid: String? = UUID.randomUUID().toString(),
+            var space_guid: String? = UUID.randomUUID().toString()
+    ) : ProvisionBody() {
 
-  data class NoPlanFieldProvisioning(
-      var plan_id: String?,
-      var organization_guid: String? = UUID.randomUUID().toString(),
-      var space_guid: String? = UUID.randomUUID().toString()
-  ) : ProvisionBody() {
+        constructor(service: Service) : this(
+                service_id = service.id
+        )
+    }
 
-    constructor(plan: Plan) : this(
-        plan_id = plan.id
-    )
-  }
+    data class NoPlanFieldProvisioning(
+            var plan_id: String?,
+            var organization_guid: String? = UUID.randomUUID().toString(),
+            var space_guid: String? = UUID.randomUUID().toString()
+    ) : ProvisionBody() {
 
-  data class NoOrgFieldProvisioning(
-      var service_id: String?,
-      var plan_id: String?,
-      var space_guid: String? = UUID.randomUUID().toString()
-  ) : ProvisionBody() {
+        constructor(plan: Plan) : this(
+                plan_id = plan.id
+        )
+    }
 
-    constructor(service: Service, plan: Plan) : this(
-        service_id = service.id,
-        plan_id = plan.id
-    )
-  }
+    data class NoOrgFieldProvisioning(
+            var service_id: String?,
+            var plan_id: String?,
+            var space_guid: String? = UUID.randomUUID().toString()
+    ) : ProvisionBody() {
 
-  data class NoSpaceFieldProvisioning(
-      var service_id: String?,
-      var plan_id: String?,
-      var organization_guid: String? = UUID.randomUUID().toString()
-  ) : ProvisionBody() {
+        constructor(service: Service, plan: Plan) : this(
+                service_id = service.id,
+                plan_id = plan.id
+        )
+    }
 
-    constructor(service: Service, plan: Plan) : this(
-        service_id = service.id,
-        plan_id = plan.id
-    )
-  }
+    data class NoSpaceFieldProvisioning(
+            var service_id: String?,
+            var plan_id: String?,
+            var organization_guid: String? = UUID.randomUUID().toString()
+    ) : ProvisionBody() {
+
+        constructor(service: Service, plan: Plan) : this(
+                service_id = service.id,
+                plan_id = plan.id
+        )
+    }
 }
--- src/main/kotlin/de/evoila/osb/checker/tests/ProvisionJUnit5.kt
+++ src/main/kotlin/de/evoila/osb/checker/tests/ProvisionJUnit5.kt
@@ -24,6 +24,7 @@ class ProvisionJUnit5 : TestBase() {
         assertTrue("404 MUST be returned if the Service Instance does not exist but was $statusCode instead")
         { statusCode.equals(404)}
     }
+
     @TestFactory
     fun runSyncTest(): List<DynamicNode> {
 
@@ -31,6 +32,7 @@ class ProvisionJUnit5 : TestBase() {
         val instanceId = UUID.randomUUID().toString()
         val service = catalog.services.first()
         val plan = service.plans.first()
+
         val provisionRequestBody = ProvisionBody.ValidProvisioning(service, plan)
         val dynamicNodes = mutableListOf<DynamicNode>()
 
@@ -38,8 +40,9 @@ class ProvisionJUnit5 : TestBase() {
                 dynamicContainer("should handle sync requests correctly", listOf(
                         dynamicTest("Sync PUT request") {
                             val statusCodePut = provisionRequestRunner.runPutProvisionRequestSync(instanceId, provisionRequestBody)
+                            print(" ::: BOdyRequest :::  " +  provisionRequestBody)
                             assertTrue("Should return  201 in case of a sync service broker or 422 if it's async but it was $statusCodePut.")
-                            { statusCodePut in listOf(201, 422) }
+                            { statusCodePut in listOf(201,200) }
                         },
                         dynamicTest("Sync Fetch request") {
                             val statusCodeFetch = provisionRequestRunner.runFetchProvisionRequestSync(instanceId)
@@ -49,7 +52,7 @@ class ProvisionJUnit5 : TestBase() {
                         dynamicTest("Sync DELETE request") {
                             val statusCodePut = provisionRequestRunner.runDeleteProvisionRequestSync(instanceId, provisionRequestBody.service_id, provisionRequestBody.plan_id)
                             assertTrue("Should return  200 in case of a sync Service Broker or 422 if it's async but it was $statusCodePut.")
-                            { statusCodePut in listOf(200, 422) }
+                            { statusCodePut in listOf(200, 410) }
                         }
                 ))
         )
